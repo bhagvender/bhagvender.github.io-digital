86eb17286417b559db998150c481d1ea0b717253
Bhagvender Singh <46966187+bhagvender@users.noreply.github.com>
2024-03-18 18:33:44 +0530

    Update init.js

--- init.js
+++ init.js
@@ -2,7 +2,7 @@
 // setup timer with date set in the future
 const timer2 = new CountdownTimer({
 	selector: "#clock2",
-	targetDate: new Date(Date.parse(new Date()) + 12 * 16 * 36 * 60 * 1000),
+	targetDate: new Date(Date.parse(new Date()) + 9 * 16 * 36 * 60 * 1000),
 	backgroundColor: "rgba(0,0,0,.15)",
 });
 
